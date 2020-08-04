921811e1d723a6b6819dd978773155fc228ef2c3
aranne <fuweiran816@gmail.com>
2020-08-04 17:03:53 -0400

    mongodb GRUD

--- boss-hiring-server/db/db_test.js
+++ boss-hiring-server/db/db_test.js
@@ -31,7 +31,60 @@ function testSave() {
   });
   user.save((err, user) => {
     if (err) return console.error(err);
-    console.log('Save', user);
+    console.log("Save", user);
   });
 }
-testSave();
+// testSave();
+
+/* Test find */
+function testFind() {
+  User.find(
+    {
+      username: "ab",
+    },
+    (err, docs) => {
+      if (err) {
+        console.log(`Error: ` + err);
+      } else {
+        if (docs.length === 0) {
+          console.log("Such user doesn't exist");
+        } else {
+          console.log("find", docs);
+        }
+      }
+    }
+  );
+}
+// testFind();
+
+/* Test update */
+function testUpdate() {
+  User.findOneAndUpdate(
+    {
+      username: "abc",
+    },
+    {
+      username: "abcd",
+    },
+    (err, doc) => {
+      if (err) {
+        console.log(`Error: ` + err);
+      } else {
+        console.log("Update" + doc);
+      }
+    }
+  );
+}
+// testUpdate();
+
+function testDelete() {
+  User.deleteOne(
+    {
+      username: "abcd",
+    },
+    (err) => {
+      console.log(`Error: ` + err);
+    }
+  );
+}
+testDelete();
