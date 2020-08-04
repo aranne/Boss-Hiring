149e612557e2a9629bad54fac42284211bd944d6
aranne <fuweiran816@gmail.com>
2020-08-04 01:34:23 -0400

    register api

--- boss-hiring-server/routes/users.js
+++ boss-hiring-server/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/register", (req, res, next) => {
+  const { username, password } = req.body;
+  if (username === "admin") {
+    res.send({
+      //  fail
+      code: 1,
+      msg: "Username already exists.",
+    });
+  } else {
+    // success
+    res.send({
+      code: 0,
+      data: {
+        id: "abc123",
+        username: username,
+        password: password,
+      },
+    });
+  }
 });
 
 module.exports = router;
