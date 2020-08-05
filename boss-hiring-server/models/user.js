69dbadb88d5a21c45f7e397b4488724c54bdff94
aranne <fuweiran816@gmail.com>
2020-08-04 23:09:52 -0400

    connect to DB

--- boss-hiring-server/models/user.js
+++ boss-hiring-server/models/user.js
@@ -1,9 +1,9 @@
-import { Schema, model } from "mongoose";
-import md5 from "blueimp-md5";
+const mongoose = require("mongoose");
+const md5 = require("blueimp-md5");
 
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
   username: { type: String, required: true }, // 用户名
-  hashed_password: { type: String, required: true }, // 密码
+  password: { type: String, required: true }, // 密码
   type: { type: String, required: true }, // 用户类型: dashen/laoban header: {type: String}, // 头像名称
   post: { type: String }, // 职位
   info: { type: String }, // 个人或职位简介
@@ -11,17 +11,5 @@ const UserSchema = new Schema({
   salary: { type: String }, // 工资
 });
 
-/**
- * Virtuals
- */
-UserSchema.virtual("password")
-  .set((password) => {
-    this._password = password;
-    this.hashed_password = md5(password);
-  })
-  .get(() => {
-    return this._password;
-  });
-
-const User = model("User", UserSchema);
-export default User;
+const User = mongoose.model("User", UserSchema);
+exports.User = User;
