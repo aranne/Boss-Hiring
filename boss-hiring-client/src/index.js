56472faaab390c71e5e03b02abf6d8497cc9de97
aranne <fuweiran816@gmail.com>
2020-08-13 05:23:38 -0400

    add less for font

--- boss-hiring-client/src/index.js
+++ boss-hiring-client/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-
 import store from "./app/store";
 import App from "./App";
-import httpService from './web/interceptors';
+import httpService from "./web/interceptors";
+import "./index.less";
 
-httpService.setupInterceptors(store);           // use http interceptor
+httpService.setupInterceptors(store); // use http interceptor
 
 ReactDOM.render(
   <React.StrictMode>
