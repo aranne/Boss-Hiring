87d6052b3c6efa5aef33a569e7dcf7b247307479
Weiran Fu <fuweiran816@gmail.com>
2020-08-16 04:18:03 -0400

    add cors

--- boss-hiring-server/config.js
+++ boss-hiring-server/config.js
@@ -1,7 +1,7 @@
 exports.dbConfig = {
   url: process.env.MONGODB_URI || "mongodb://localhost:27017/boss-hiring",
-}
+};
 
 exports.clientConfig = {
-  url: 'http://localhost:3000',
-}
+  url: "https://aranne.github.io",
+};
