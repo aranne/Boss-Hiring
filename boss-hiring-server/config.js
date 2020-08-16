65cc8695d45a75340844ceb66b1dca27247f8cf3
Weiran Fu <fuweiran816@gmail.com>
2020-08-16 01:17:07 -0400

    add env variables

--- boss-hiring-server/config.js
+++ boss-hiring-server/config.js
@@ -1,11 +1,7 @@
 exports.dbConfig = {
-  url: "mongodb://localhost:27017/boss-hiring",
+  url: process.env.MONGODB_URI || "mongodb://localhost:27017/boss-hiring",
 }
 
 exports.clientConfig = {
   url: 'http://localhost:3000',
-}
-
-exports.wsServerConfig = {
-  port: 8000,
 }
