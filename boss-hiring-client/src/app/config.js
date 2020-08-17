211a643c770e08ecc5d0112b87274bd15ef782d6
Weiran Fu <fuweiran816@gmail.com>
2020-08-17 01:12:58 -0400

    add config

--- boss-hiring-client/src/app/config.js
+++ boss-hiring-client/src/app/config.js
@@ -1,7 +1,7 @@
 export const serverConfig = {
-  url: "https://boss-hiring-app.herokuapp.com" || "http://localhost:4000",
+  url: process.env.SEVER_URI || "http://localhost:4000",
 };
 
 export const socketIOConfig = {
-  url: "https://boss-hiring-app.herokuapp.com" || "ws://localhost:4000",
+  url: process.env.SOCKETIO_URI || "ws://localhost:4000",
 };
